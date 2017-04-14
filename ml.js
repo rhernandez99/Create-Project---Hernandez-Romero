f2f003d9acf5cd6be93ba28bc6939c1ab3a4b833
Richard Hernandez <richardhernandez@Richards-MacBook-Pro.local>
2017-04-14 16:24:29 -0700

    period fixed in sentence

--- ml.js
+++ ml.js
@@ -2,6 +2,6 @@ function displayStory (a, b, c, story) {
     var a = textboxA.value;
     var b = textboxB.value;
     var c = textboxC.value;
-    var story = "Once there was " + a + " programmer who used JavaScript to " + b + " the " + c + " .";
+    var story = "Once there was " + a + " programmer who used JavaScript to " + b + " the " + c + ".";
    document.write(story);
 }
