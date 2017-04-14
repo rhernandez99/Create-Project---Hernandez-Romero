38e8520eea73af5215e542f2b1ec174a7ccefa52
Richard Hernandez <richardhernandez@Richards-MacBook-Pro.local>
2017-04-14 16:43:38 -0700

    new if conditional for function

--- ml.js
+++ ml.js
@@ -3,7 +3,13 @@ function displayStory (a, b, c, story) {
     var b = textboxB.value;
     var c = textboxC.value;
     var story = "Once there was " + a + " programmer who used JavaScript to " + b + " the " + c + ".";
-    if (a || b || c == "") {
+    if (a == "") {
+        document.write("Error");
+    }
+    else if (b == "") {
+        document.write("Error");
+    }
+    else if (c == "") {
         document.write("Error");
     } else {
       document.write(story);
