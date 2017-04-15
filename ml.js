bc2a4b0c4ab6937fde410da49c63cd66c56e1110
Richard Hernandez <richardhernandez@Richards-MacBook-Pro.local>
2017-04-14 22:05:23 -0700

    commit

--- ml.js
+++ ml.js
@@ -2,7 +2,7 @@ function displayStory (a, b, c, story) {
     var a = textboxA.value;
     var b = textboxB.value;
     var c = textboxC.value;
-    var story = "Once there was a/an" + a + " programmer who used JavaScript to " + b + " the " + c + ".";
+    var story = "Once there was a/an " + a + " programmer who used JavaScript to " + b + " the " + c + ".";
     if (a == "") {
         document.write("Error - You left a text box blank");
     }
