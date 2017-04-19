5b2c84a43632d2d41ddfa7084a6815a67e0ec0e9
Jorge Romero <jorgeromero@Jorges-MacBook-Air.local>
2017-04-18 20:56:19 -0700

    ml is version 1 and mlv2 is version 2. Work on mlv2...if code doesn't work anymore you have ml

--- mlv2.js
+++ mlv2.js
@@ -4,11 +4,15 @@ function add () {
  return counter += 1;
 }
 
-function displayStory(a, b, c, story) {
+function displayStory(a, b, c, story, clickcount) {
     var a = textboxA.value;
     var b = textboxB.value;
     var c = textboxC.value;
     var story = "Once there was a/an " + a + " programmer who used JavaScript to " + b + " the " + c + ".";
+    if (clickcount > 5){
+        return pass;
+}
+    else
     if (a == "") {
         $('#story').append('<br/>' + "Error - You left a text box blank");
     }
@@ -24,6 +28,8 @@ function displayStory(a, b, c, story) {
 }
 
 function myFunction(){
-    document.getElementById("counterClick").innerHTML = add();
+    var clickCount = add();
+    document.getElementById("counterClick").innerHTML = clickCount;
+
 }
 
