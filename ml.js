e3be781badd9cce4776c7fea712c04201508e936
Richard Hernandez <richardhernandez@Richards-MacBook-Pro.local>
2017-04-18 23:16:56 -0700

    very new code with bootstrap

--- ml.js
+++ ml.js
@@ -1,29 +1,18 @@
-var counter = 0;
-
-function add () {
- return counter += 1;
-}
-
 function displayStory(a, b, c, story) {
     var a = textboxA.value;
     var b = textboxB.value;
     var c = textboxC.value;
     var story = "Once there was a/an " + a + " programmer who used JavaScript to " + b + " the " + c + ".";
     if (a == "") {
-        $('#story').append('<br/>' + "Error - You left a text box blank");
+        $('#estory').append('<br/>' + "Error - You left a text box blank");
     }
     else if (b == "") {
-        $('#story').append('<br/>' + "Error - You left a text box blank");
+        $('#estory').append('<br/>' + "Error - You left a text box blank");
     }
     else if (c == "") {
-        $('#story').append('<br/>' + "Error - You left a text box blank");
+        $('#estory').append('<br/>' + "Error - You left a text box blank");
     }
     else {
         $('#story').append('<br/>' + story);
     }
 }
-
-function myFunction(){
-    document.getElementById("counterClick").innerHTML = add();
-}
-
